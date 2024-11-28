a38e38092fc34ad524e4ea7e0c49cfdd330d406c
Mireille El Haddad <45281188+mireillehaddad@users.noreply.github.com>
2024-11-28 02:27:00 +0000

    Adding a conversational memory to chainlit didn't work in this version. Work on fixing it!

---
+++ chat_chainlit_conv_mem.py
@@ -0,0 +1,62 @@
+import chainlit as cl
+
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+
+
+def get_prompt(instruction: str, history: list[str] | None = None) -> str:
+    system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if len(history) > 0:
+        prompt += f"This is the conversation history: {''.join(history)}. Now answer the question: "
+    prompt += f"{instruction}\n\n### Response:\n"
+    return prompt
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history = cl.user_session.get("message_history")
+    # response = f"Hello, you just sent: {message.content}!"
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)
+
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+        "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+    )
+    print("A new chat session has started!")
+
+
+'''
+history = []
+
+question = "Which city is the capital of India?"
+prompt = get_prompt(question)
+answer = ""
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+    answer += word
+print()
+history.append(answer)
+
+question = "And of the United States?"
+prompt = get_prompt(question, history)
+for word in llm(prompt, stream=True):
+    print(word, end="", flush=True)
+print()
+'''
